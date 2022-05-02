d3a1912f52d64860aa0ab17d67ec7b3386558aa2
Šimon Vyhnis <simon.vyhnis@gmail.com>
2022-05-02 19:28:43 +0200

    Simple point dragging

--- src/GpxRoute.kt
+++ src/GpxRoute.kt
@@ -1,45 +1,74 @@
 import org.simpleframework.xml.Serializer
 import org.simpleframework.xml.core.Persister
 import java.awt.Graphics
+import java.awt.event.MouseEvent
+import java.awt.event.MouseMotionListener
 import java.io.File
 
-class GpxRoute(path: String){
+class GpxRoute(path: String) : MouseMotionListener{
     private val latLimits = DoubleLimits()
     private val lonLimits = DoubleLimits()
-    private lateinit var points : List<Trkpt>
+    private var points = ArrayList<TrackPoint>()
+    private val calibrationPoints = ArrayList<CalibrationPoint>()
+    private var isPointSelected = false
 
     init{
         val serializer: Serializer = Persister()
         val file = File(path)
         val data = serializer.read(GpxFile::class.java, file)
-        points = data.trk.points
 
-
-        for (point in points){
+        for (point in data.trk.points){
             point.lat?.let { latLimits.tryNew(it) }
             point.lon?.let { lonLimits.tryNew(it) }
         }
-    }
-
-
-    fun draw(g: Graphics) {
-        val latScale = latLimits.getDelta()
-        println(latScale)
-        val lonScale = lonLimits.getDelta()
 
         val width = 500
         val height = 500
         val padding = 50
 
-        for (point in points){
-
-            g.drawRect(((point.lon!!-lonLimits.min!!)/lonScale*(width-2*padding)).toInt()+padding, ((point.lat!!-latLimits.min!!)/latScale*(height-2*padding)).toInt()+padding, 1,1)
-            println(((point.lon!!-lonLimits.min!!)/lonScale*400).toInt())
+        for (point in data.trk.points){
+            points.add(TrackPoint(
+                ((point.lat!!-latLimits.min!!)/latLimits.getDelta()*(height-(2*padding))).toInt(),
+                ((point.lon!!-lonLimits.min!!)/lonLimits.getDelta()*(width-(2*padding))).toInt()))
         }
-
     }
 
 
+    fun draw(g: Graphics) {
+        for(i: Int in 0..points.size-2){
+            var point1 = points[i]
+            var point2 = points[i+1]
+            for(calibrationPoint in calibrationPoints){
+                if(calibrationPoint.pos == i){
+                    point1 = TrackPoint(calibrationPoint.x, calibrationPoint.y)
+                }else if(calibrationPoint.pos == i+1){
+                    point2 = TrackPoint(calibrationPoint.x, calibrationPoint.y)
+                }
+            }
+            g.drawLine(point1.x, point1.y, point2.x, point2.y)
+        }
+    }
+
+    override fun mouseDragged(e: MouseEvent?) {
+        if (e != null) {
+            if (isPointSelected) {
+                calibrationPoints[calibrationPoints.size - 1].x = e.x
+                calibrationPoints[calibrationPoints.size - 1].y = e.y
+            } else {
+                for (i: Int in 0 until points.size) {
+                    if (points[i].x < e.x+5 && points[i].x > e.x-5 &&
+                        points[i].y < e.y+5 && points[i].y > e.y-5){
+                        calibrationPoints.add(CalibrationPoint(i, e.x, e.y))
+                        isPointSelected = true
+                    }
+                }
+            }
+        }
+    }
+
+    override fun mouseMoved(e: MouseEvent?){
+        isPointSelected = false
+    }
 }
 
 class DoubleLimits{
@@ -64,4 +93,7 @@ class DoubleLimits{
         return max!!-min!!
     }
 }
+
+class TrackPoint(var x: Int, var y: Int)
+class CalibrationPoint(var pos : Int, var x: Int, var y: Int)
 
--- src/Window.kt
+++ src/Window.kt
@@ -3,12 +3,11 @@ import javax.swing.JFrame
 
 
     fun main(){
-        val gpxRoute = GpxRoute("example_data/klasika.gpx")
+        val gpxRoute = GpxRoute("example_data/kratka.gpx")
         val canvas = object : Canvas() {
             override fun paint(g: Graphics) {
                 // set color to red
-                g.color = Color.red
-                g.drawRect(0,0,10,10)
+                g.color = Color.green
                 gpxRoute.draw(g)
             }
         }
@@ -18,6 +17,11 @@ import javax.swing.JFrame
         frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
         frame.size = Dimension(500, 500)
         frame.setLocationRelativeTo(null)
+        canvas.addMouseMotionListener(gpxRoute)
         frame.isVisible = true
+        while(true){
+            canvas.repaint()
+            Thread.sleep(100)
+        }
 
     }
